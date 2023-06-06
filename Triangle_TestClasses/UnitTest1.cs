9d46ed2cb2ca2d36744cf29517318ca199f71c69
Jaydeep Nadoda <Jnadoda5939@conestogac.on.ca>
2023-06-06 14:39:34 -0400

    Test Number 1 for Equilateral Triangle

--- Triangle_TestClasses/UnitTest1.cs
+++ Triangle_TestClasses/UnitTest1.cs
@@ -1,14 +1,37 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 using System;
+using TriangleSolver;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 
 namespace Triangle_TestClasses
 {
-    [TestClass]
-    public class UnitTest1
+    [TestFixture]
+    public class Tests_For_EQUILATERAL
     {
-        [TestMethod]
-        public void TestMethod1()
+        [Test]
+        public void ValideTringle_InputTestForEquilateral()
         {
+            //Arragne
+            int firstAngle = 60;
+            int secondAngle = 60;
+            int thirdAngle = 60;
+
+
+            string expected = "An EQUILATERAL triangle is formed";
+
+            //Act
+            string actual = Triangle.AnalyzeTriangle(firstAngle, secondAngle, thirdAngle);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
         }
+
     }
+
+   
+
 }
+
+
